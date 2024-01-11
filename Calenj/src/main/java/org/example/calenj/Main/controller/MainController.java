8f6a11427bbbf88c5fabef2137b1951d44a6cb2f
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-01-11 10:34:12 +0900

    2021.01.11 Test

--- Calenj/src/main/java/org/example/calenj/Main/controller/MainController.java
+++ Calenj/src/main/java/org/example/calenj/Main/controller/MainController.java
@@ -1,7 +1,8 @@
 package org.example.calenj.Main.controller;
 
 import org.example.calenj.Main.model.MainService;
-import org.example.calenj.Main.domain.User;
+
+import org.example.calenj.domain.Test2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,19 +32,12 @@ public class MainController {
     @GetMapping("/api/dbmsTest")
     @ResponseBody
     public String dbTest() {
-        //성공
-        User user = new User();
+        Test2 test = new Test2();
+        test.setAccount_id("dysj12");
+        test.setUser_password("dysj1234");
 
-        user.setAccount_id("kosq3964");
-        user.setKakao_login(false);
-        user.setNaver_login(false);
-        user.setWithdrawed(false);
-        user.setUser_password("1234");
-        user.setUser_roll("user");
-        user.setUser_email("kosq3964@naver.com");
-        user.setUser_phone("01025023964");
-        user.setUser_join_date("2000-11-11");
-        mainService.saveUser(user);
-        return user.toString();
+        mainService.test(test);
+
+        return "연습했어영";
     }
 }
--- Calenj/src/main/java/org/example/calenj/Main/model/MainService.java
+++ Calenj/src/main/java/org/example/calenj/Main/model/MainService.java
@@ -1,9 +1,9 @@
 package org.example.calenj.Main.model;
 
-import org.example.calenj.Main.Repository.TestRepository;
+import org.example.calenj.Main.Repository.Test2Repository;
 import org.example.calenj.Main.Repository.UserRepository;
-import org.example.calenj.Main.domain.Test;
-import org.example.calenj.Main.domain.User;
+import org.example.calenj.domain.Test2;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +14,13 @@ public class MainService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private TestRepository testRepository; //리포지토리는 맵퍼를 포함 (맵퍼의 상위호환)
+    private Test2Repository test2Repository;
 
-    //유저 세이브 (save)는 jpa 기본 제공 메소드
-    public void saveUser(User user){
-        System.out.println( "user : " + user.toString());
-        userRepository.save(user);
+
+
+    public void test(Test2 test){
+        test2Repository.save(test);
     }
-    //테스트 세이브 (save)는 jpa 기본 제공 메소드
-    public void test(Test test){
-        System.out.println( "test : " + test.toString());
-        testRepository.save(test);
-    }
+
+
 }
