bceb8737966c8080c29033c33df7b01e9dfcaf8e
kwontaein <zodls1128@gmail.com>
2024-01-11 11:08:03 +0900

    Kwon_branch

--- Calenj/src/main/java/org/example/calenj/Main/model/MainService.java
+++ Calenj/src/main/java/org/example/calenj/Main/model/MainService.java
@@ -4,6 +4,7 @@ import org.example.calenj.Main.Repository.Test2Repository;
 import org.example.calenj.Main.Repository.UserRepository;
 import org.example.calenj.Main.domain.Test2;
 
+import org.example.calenj.Main.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +23,9 @@ public class MainService {
         test2Repository.save(test);
     }
 
-
+    //유저 세이브 (save)는 jpa 기본 제공 메소드
+    public void saveUser(User user){
+        System.out.println( "user : " + user.toString());
+        userRepository.save(user);
+    }
 }
