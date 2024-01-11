b37ddc3979fb3f823992150f10b56e5482ec9296
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-01-11 10:39:38 +0900

    2021.01.11 Test

---
+++ Calenj/src/main/java/org/example/calenj/Main/Repository/Test2Repository.java
@@ -0,0 +1,9 @@
+package org.example.calenj.Main.Repository;
+
+import org.example.calenj.domain.Test2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+//dependency로 의존성 관계를 위한 Jpa저장소를 상속받는 인터페이스 생성
+public interface Test2Repository extends JpaRepository<Test2, Integer> {
+}
---
+++ Calenj/src/main/java/org/example/calenj/domain/Test2.java
@@ -0,0 +1,22 @@
+package org.example.calenj.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.processing.Generated;
+
+@Entity //테이블 어노테이션
+@Getter
+@Setter
+public class Test2 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private int userid; //-> 얜 그냥 구분하기 위한 값
+
+    @Column
+    private String account_id;
+    private String user_password;
+
+}
