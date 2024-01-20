061dd7aa5185092b5309a0356d1404c3c26afc2f
kwontaein <zodls1128@gmail.com>
2024-01-20 16:01:37 +0900

    엔티티 재정의2 2024_01_20

--- Calenj/src/main/java/org/example/calenj/Main/domain/Group/Group_VoteEntity.java
+++ Calenj/src/main/java/org/example/calenj/Main/domain/Group/Group_VoteEntity.java
@@ -8,4 +8,8 @@ import lombok.*;
 @Getter
 @DiscriminatorValue("Group_Vote") // 서브 테이블을 판별하기 위한 값
 public class Group_VoteEntity extends GroupEntity {
+    private String vote_title;
+    private String vote_Item;
+    private String vote_start_date;
+    private String vote_end_date;
 }
