b5ae0d494cb72e20c1f988b686f41481f2130724
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-01-20 16:12:05 +0900

    2024.01.20 table define2

--- Calenj/src/main/java/org/example/calenj/Main/domain/Group/Group_UserEntity.java
+++ Calenj/src/main/java/org/example/calenj/Main/domain/Group/Group_UserEntity.java
@@ -2,14 +2,23 @@ package org.example.calenj.Main.domain.Group;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
+import org.example.calenj.Main.domain.UserEntity;
 
 @Entity(name = "Group_User")
 @Getter
 @DiscriminatorValue("Group_User") // 서브 테이블을 판별하기 위한 값
 public class Group_UserEntity extends GroupEntity {
-    private long group_id;
-    private long user_id;
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private GroupEntity group;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
     private String role;
     private String group_user_location;
 }
