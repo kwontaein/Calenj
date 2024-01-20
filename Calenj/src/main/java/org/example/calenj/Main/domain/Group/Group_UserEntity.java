ede249be472fb742fbad979dc14675942fc0117c
kwontaein <zodls1128@gmail.com>
2024-01-20 16:12:03 +0900

    a 2024_01_20

--- Calenj/src/main/java/org/example/calenj/Main/domain/Group/Group_UserEntity.java
+++ Calenj/src/main/java/org/example/calenj/Main/domain/Group/Group_UserEntity.java
@@ -2,11 +2,23 @@ package org.example.calenj.Main.domain.Group;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import lombok.*;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import org.example.calenj.Main.domain.UserEntity;
 
 @Entity(name = "Group_User")
 @Getter
 @DiscriminatorValue("Group_User") // 서브 테이블을 판별하기 위한 값
 public class Group_UserEntity extends GroupEntity {
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private GroupEntity group;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    private String role;
+    private String group_user_location;
 }
