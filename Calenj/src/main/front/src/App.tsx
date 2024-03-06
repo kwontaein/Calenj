cc6e8574cabe535713ed8379b6f8babde2949bdf
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-03-06 23:17:05 +0900

    03.06 git merge upstream/origin master

--- Calenj/src/main/front/src/App.tsx
+++ Calenj/src/main/front/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Home from './Home';
-import Rooms from './Test/Rooms';
 import SignUp from './components/Auth/Sign_up';
 import Sign from './components/Auth/Sign';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MakeGroup from "./components/Group/MakeGroup";
 
- const App:React.FC =()=> {
+const App:React.FC =()=> {
 
     return (
         <div className="App">
@@ -16,7 +15,6 @@ import MakeGroup from "./components/Group/MakeGroup";
                     <Route path={"/signup"} element={<SignUp/>}></Route>
                     <Route path={"/sign"} element={<Sign/>}></Route>
                     <Route path={"/group/makegroup"} element={<MakeGroup/>}></Route>
-                    <Route path={"/example"} element={<Rooms/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
