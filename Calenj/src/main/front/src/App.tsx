125caa92ec7bcaf69e393d7b1ca017eeac97b528
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-03-15 16:43:46 +0900

    03.15 git checkout

--- Calenj/src/main/front/src/App.tsx
+++ Calenj/src/main/front/src/App.tsx
@@ -22,9 +22,9 @@ const App: React.FC = () => {
 export default App;
 
 export function dofilter (error:string){
-    if (error === "NON_EXISTENT_ERROR") {
-        window.alert("존재하지 않는 아이디 입니다. 다시 확인해주세요.")
-    } else if (error === "PW_ERROR") {
-        window.alert("비밀번호가 틀렸습니다. 다시 입력해주세요.")
+    if (error === "ALL_TOKEN_EXPIRED") {
+        window.alert("모든 토큰이 만료되었습니다. 재로그인하세요.")
+    } else if (error === "UNKNOWN_EXCEPTION") {
+        window.alert("모든 토큰이 만료되었습니다. 재로그인하세요.")
     }
 }
--- Calenj/src/main/front/src/Test/JustSend.tsx
+++ Calenj/src/main/front/src/Test/JustSend.tsx
@@ -20,10 +20,10 @@ const JustSend = () => {
                 console.log(error)
                 console.log(error.response.status)
                 console.log(error.response.data)
-                if (error.response.data === "NON_EXISTENT_ERROR") {
-                    window.alert("존재하지 않는 아이디 입니다. 다시 확인해주세요.")
-                } else if (error.response.data === "PW_ERROR") {
-                    window.alert("비밀번호가 틀렸습니다. 다시 입력해주세요.")
+                if (error.response.data === "ALL_TOKEN_EXPIRED") {
+                    window.alert("모든 토큰이 만료되었습니다. 재로그인하세요.")
+                } else if (error.response.data === "UNKNOWN_EXCEPTION") {
+                    window.alert("모든 토큰이 만료되었습니다. 재로그인하세요.")
                 }
             })
     }, []);
