40f499a6c990d7edb749d94ecc0d9a0be2121b9e
kwontaein <zodls1128@gmail.com>
2024-04-02 23:53:34 +0900

    2024_04_02 엔티티 및 리포지토리 수정

--- Calenj/src/main/java/org/example/calenj/Main/Repository/FriendRepository.java
+++ Calenj/src/main/java/org/example/calenj/Main/Repository/FriendRepository.java
@@ -2,6 +2,7 @@ package org.example.calenj.Main.Repository;
 
 import org.example.calenj.Main.DTO.FriendDTO;
 import org.example.calenj.Main.domain.FriendEntity;
+import org.example.calenj.Main.domain.Ids.FriendId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,9 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-public interface FriendRepository extends JpaRepository<FriendEntity, UUID> {
+public interface FriendRepository extends JpaRepository<FriendEntity, FriendId> {
     @Query("SELECT new org.example.calenj.Main.DTO.FriendDTO(f.friendId,f.ownUserId.userEmail,f.friendUserId,f.nickName,f.createDate" +
             ",f.status) FROM Friends f WHERE f.ownUserId.userEmail =:userId")
     Optional<List<FriendDTO>> findFriendListById(String userId);
--- Calenj/src/main/java/org/example/calenj/Main/Repository/Group/Group_ScheduleRepository.java
+++ Calenj/src/main/java/org/example/calenj/Main/Repository/Group/Group_ScheduleRepository.java
@@ -2,6 +2,7 @@ package org.example.calenj.Main.Repository.Group;
 
 import org.example.calenj.Main.DTO.Group.GroupScheduleDTO;
 import org.example.calenj.Main.domain.Group.GroupScheduleEntity;
+import org.example.calenj.Main.domain.Ids.GroupScheduleId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface Group_ScheduleRepository extends JpaRepository<GroupScheduleEntity, UUID> {
+public interface Group_ScheduleRepository extends JpaRepository<GroupScheduleEntity, GroupScheduleId> {
     // 다섯 번째 쿼리: GroupScheduleEntity 조회
     @Query("SELECT new org.example.calenj.Main.DTO.Group.GroupScheduleDTO(gs.groupScheduleTitle, gs.groupScheduleContent, gs.groupScheduleLocation, gs.groupScheduleId) FROM Group_Schedule gs WHERE gs.groupUser.group.groupId = :groupId")
     List<GroupScheduleDTO> findGroupSchedule(@Param("groupId") UUID groupId);
