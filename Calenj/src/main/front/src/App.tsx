8ee5a6ae570d61e2c236819daf631c8a18c7339f
kwontaein <zodls1128@gmail.com>
2024-04-20 03:52:20 +0900

    2024_04_20 네이버 지도 추가

--- Calenj/src/main/front/src/App.tsx
+++ Calenj/src/main/front/src/App.tsx
@@ -1,8 +1,7 @@
 import Home from './Home';
 import SignUp from './components/Auth/Sign_up';
 import Sign from './components/Auth/Sign';
-import {BrowserRouter, Routes, Route, useParams} from 'react-router-dom';
-import GroupDetail from "./components/Group/GroupDetail";
+import {BrowserRouter, Routes, Route,} from 'react-router-dom';
 import NoticeDetail from './components/Group/Notice/NoticeDetail';
 import VoteDetail from './components/Group/Vote/VoteDetail';
 import InviteGroup from "./components/Group/InviteGroup";
@@ -10,22 +9,20 @@ import FriendList from "./components/Friends/FriendList";
 import NaverMap from "./components/Group/Map/NaverMap"
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import stompReducer, {
+import {
     DispatchStompProps,
     mapDispatchToStompProps,
     StompData,
     mapStateToStompProps
 } from './store/module/StompReducer';
 import {connect} from "react-redux";
-import {useQuery, useMutation, useQueryClient, UseQueryResult} from '@tanstack/react-query';
+import {useQuery,} from '@tanstack/react-query';
 import {sagaMutation} from './store/store'
 import RequestFriend from "./components/Friends/RequestFriend";
-import DefaultNaviation from './DefaultNavigation'
 
 //대표 색 : #007bff
 export const QUERY_COOKIE_KEY: string = 'cookie';
 
-// import GroupList from "./components/Group/GroupList";
 
 interface SubScribe {
     groupId: string;
@@ -34,10 +31,9 @@ interface SubScribe {
 
 
 const App: React.FC<DispatchStompProps & StompData> = ({synchronizationStomp, updateOnline, stomp, updateLoading}) => {
-    const queryClient = useQueryClient();
     const [loading, setLoading] = useState<boolean>(false);
 
-    //api를 통하여 쿠키를 post하여 boolean값을 return 받는다.
+    //api 를 통하여 쿠키를 post 하여 boolean 값을 return 받는다.
     //accessToken 만료 시 refreshToken 체크 후 재발급, 모든 토큰 만료 시 재로그인 필요
     const checkCookie = async (): Promise<boolean> => {
         const response = await axios.post('/api/postCookie');
@@ -74,17 +70,17 @@ const App: React.FC<DispatchStompProps & StompData> = ({synchronizationStomp, up
     }, [stomp.loading])
 
     function subScribeFilter(friendList: string[], groupList: string[], userId: string) {
-        let parmasList = [];
-        parmasList.push([userId]) //친구요청
-        parmasList.push(groupList) //그룹채팅
-        parmasList.push(friendList) //친구채팅
-        return parmasList;
+        let paramsList = [];
+        paramsList.push([userId]) //친구요청
+        paramsList.push(groupList) //그룹채팅
+        paramsList.push(friendList) //친구채팅
+        return paramsList;
     }
 
     // //v5이후로 인자를 객체 형태로 전달해야함
-    const logState = useQuery<boolean, Error>({
+    useQuery<boolean, Error>({
         queryKey: [QUERY_COOKIE_KEY],
-        queryFn: checkCookie, //HTTP 요청함수 (Promise를 반환하는 함수)
+        queryFn: checkCookie, //HTTP 요청함수 (Promise 를 반환하는 함수)
     });
 
     return (
