8044c4dfd235eb7fbada179f983395e9168afed7
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-04-27 18:17:10 +0900

    04.27 파일경로 셋팅 전 push

--- Calenj/src/main/front/src/Home.tsx
+++ Calenj/src/main/front/src/Home.tsx
@@ -1,13 +1,8 @@
-import {QueryClient, useQueryClient} from '@tanstack/react-query';
 import React, {useLayoutEffect, useState, useRef, useEffect} from 'react'
-import FriendList from "./components/Friends/FriendList";
 import {connect} from 'react-redux'
 import DefaultNavigation from "./components/Global/DefaultNavigation";
-import {FullScreen_div, RowFlexBox} from "./style/FormStyle";
-import EventManagementBar from "./components/Global/EventManagementBar";
-import SubNavigationbar from "./components/Global/SubNavigationbar";
-import GroupDetail from "./components/Group/GroupDetail";
-import {Content_Container} from "./style/Navigation/DefaultNavigationStyle";
+import {FullScreen_div} from "./style/FormStyle";
+import NavigationComposition from './components/Global/NavigationComposition'
 import {
     NavigateState,
     DispatchNavigationProps,
@@ -18,29 +13,11 @@ import {
 import SignState from "./components/Auth/SignState";
 const Home: React.FC<NavigateState &DispatchNavigationProps> = ({navigateInfo}) => {
 
-    useEffect(() => {
-        if(navigateInfo.navigate ==="group"){
-            console.log(`redirectDetail ${navigateInfo.navigateParam}`)
-        }
-
-    }, [navigateInfo]);
-
     return (
         <FullScreen_div style={{display:"flex", flexDirection:"row"}}>
             {/*<SignState/>*/}
             <DefaultNavigation/>
-
-            <Content_Container>
-                <EventManagementBar navigate={navigateInfo.navigate}/>
-                <RowFlexBox style={{height:'calc(100% - 51px)'}}>
-                    <SubNavigationbar  navigate={navigateInfo.navigate}/>
-                    {(navigateInfo.navigate === "group" && navigateInfo.navigateParam !== '') &&
-                    <GroupDetail groupId={navigateInfo.navigateParam}/>
-                    }
-                </RowFlexBox>
-
-            </Content_Container>
-
+            <NavigationComposition/>
         </FullScreen_div>
     )
 }
