1c9b840cd0859d8850414d65df60cd80aa93c773
kwontaein <zodls1128@gmail.com>
2024-04-28 16:49:34 +0900

    2024_04_28 재업로드

--- Calenj/src/main/front/src/Home.tsx
+++ Calenj/src/main/front/src/Home.tsx
@@ -10,19 +10,18 @@ import {
     mapStateToStompProps
 } from './store/module/StompReducer';
 import SignState from "./components/Auth/SignState";
-const Home: React.FC<StompData &DispatchStompProps> = ({stomp}) => {
+
+const Home: React.FC<StompData & DispatchStompProps> = ({stomp}) => {
 
     return (
-        <FullScreen_div style={{display:"flex", flexDirection:"row"}}>
-            {stomp.isOnline ?
-                <FullScreen_div style={{display:"flex", flexDirection:"row"}}>
+        <FullScreen_div style={{display: "flex", flexDirection: "row"}}>
+            {stomp.isOnline === "ONLINE" ?
+                <FullScreen_div style={{display: "flex", flexDirection: "row"}}>
                     <DefaultNavigation/>
                     <NavigationComposition/>
                 </FullScreen_div> :
                 <SignState/>}
-
-
         </FullScreen_div>
     )
 }
-export default connect(mapStateToStompProps,mapDispatchToStompProps) (Home)
+export default connect(mapStateToStompProps, mapDispatchToStompProps)(Home)
