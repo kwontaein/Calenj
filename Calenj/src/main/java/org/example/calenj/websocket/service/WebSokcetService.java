ac5206ac8278a9f7188dedd950024c343d905b79
kwontaein <zodls1128@gmail.com>
2024-04-29 22:08:12 +0900

    2024_04_29 온라인 유저 목록 반환

--- Calenj/src/main/java/org/example/calenj/websocket/service/WebSokcetService.java
+++ Calenj/src/main/java/org/example/calenj/websocket/service/WebSokcetService.java
@@ -1,10 +1,16 @@
 package org.example.calenj.websocket.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.calenj.global.service.GlobalService;
+import org.example.calenj.user.domain.UserEntity;
+import org.example.calenj.user.repository.UserRepository;
 import org.example.calenj.websocket.dto.request.AlarmRequest;
 import org.example.calenj.websocket.dto.request.ChatMessageRequest;
-import org.example.calenj.user.repository.UserRepository;
-import org.example.calenj.user.domain.UserEntity;
+import org.example.calenj.websocket.dto.response.ChatMessageResponse;
+import org.example.calenj.websocket.dto.response.OnlineUserResponse;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +20,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -26,6 +30,11 @@ import java.util.stream.Collectors;
 public class WebSokcetService {
 
     private final UserRepository userRepository;
+    private final GlobalService globalService;
+    private final SimpMessagingTemplate template; //특정 Broker로 메세지를 전달
+    private final SimpUserRegistry simpUserRegistry;
+    private final OnlineUserResponse onlineUserResponse;
+
 
     public String returnNickname(Authentication authentication) {
         UserEntity userEntity = userRepository.findByUserEmail(authentication.getName()).orElseThrow(() -> new RuntimeException("존재하지 않는 정보"));
@@ -179,8 +188,117 @@ public class WebSokcetService {
         return line -> !line.contains("EndPoint") && !line.contains(param);
     }
 
-    public void OnlineChange(AlarmRequest request, String userEmail) {
-        userRepository.updateIsOnline(userEmail, request.getOnlineState());
+
+    public void defaultSend(Authentication authentication, ChatMessageRequest message, String target) {
+
+        String username = returnNickname(authentication);
+        String userEmail = returnEmail(username);
+        String nowTime = globalService.nowTime();
+
+        message.setNickName(username);
+        message.setUserEmail(userEmail);
+        message.setSendDate(nowTime);
+
+        ChatMessageResponse response = filterNullFields(message);
+        getUsers("topic/" + target + message.getParam());
+
+        //알림 갯수 반환
+        if (message.getState() == ChatMessageRequest.fileType.ALARM) {
+
+            try {
+
+                // 파일로부터 채팅 내용을 읽어와서 보내기
+                int setPoint = countLinesUntilEndPoint(message);
+                response.setEndPoint(setPoint);
+                template.convertAndSendToUser(response.getUserEmail(), "/topic/" + target + "/" + response.getParam(), response);
+
+            } catch (Throwable e) {
+
+                // 에러가 발생할 경우.
+                e.printStackTrace();
+
+            }
+            // State가 1이라면 일반 메시지
+        } else if (message.getState() == ChatMessageRequest.fileType.READ) {
+
+            List<String> file = readGroupChattingFile(message);
+            response.setMessage(file);
+            template.convertAndSendToUser(response.getUserEmail(), "/topic/" + target + "/" + response.getParam(), response);
+
+        } else if (message.getState() == ChatMessageRequest.fileType.RELOAD) {
+
+            List<String> file = readGroupChattingFileSlide(message);
+            response.setMessage(file);
+            template.convertAndSendToUser(response.getUserEmail(), "/topic/" + target + "/" + response.getParam(), response);
+
+        } else if (message.getState() == ChatMessageRequest.fileType.SEND) {
+
+            saveChattingToFile(message);
+            response.setMessage(Collections.singletonList(message.getMessage()));
+            response.setChatUUID(message.getChatUUID());
+            System.out.println(response);
+            template.convertAndSend("/topic/" + target + "/" + response.getParam(), response);
+
+            //2라면 나갈 때 엔드포인트 설정
+        } else if (message.getState() == ChatMessageRequest.fileType.ENDPOINT) {
+
+            //파일에 엔드포인트 저장
+            saveChattingToFile(message);
+        }
+    }
+
+    public void personalEvent(Authentication authentication, AlarmRequest request) {
+        String username = returnNickname(authentication);
+        String userEmail = returnEmail(username);
+    }
+
+    // null이 아닌 필드만 포함시키는 메소드
+    private ChatMessageResponse filterNullFields(ChatMessageRequest request) {
+        ChatMessageResponse filteredResponse = new ChatMessageResponse();
+        if (request.getParam() != null) {
+            filteredResponse.setParam(request.getParam());
+        }
+        if (request.getMessage() != null) {
+            filteredResponse.setMessage(Collections.singletonList(request.getMessage()));
+        }
+        if (request.getNickName() != null) {
+            filteredResponse.setNickName(request.getNickName());
+        }
+        if (request.getUserEmail() != null) {
+            filteredResponse.setUserEmail(request.getUserEmail());
+        }
+        if (request.getSendDate() != null) {
+            filteredResponse.setSendDate(request.getSendDate());
+        }
+
+        filteredResponse.setState(request.getState());
+        filteredResponse.setEndPoint(request.getEndPoint());
+        // 필요한 필드들을 추가로 확인하여 null이 아닌 것만 설정
+        return filteredResponse;
+    }
+
+    public void getUsers(String param) {
+        Set<SimpUser> simpUsers = simpUserRegistry.getUsers();
+
+        Set<String> filteredUserNames = simpUsers.stream()
+                .filter(simpUser -> simpUser.getSessions().stream()
+                        .anyMatch(session -> session.getSubscriptions().stream()
+                                .anyMatch(subscription -> subscription.getDestination().contains(param))))
+                .map(SimpUser::getName)
+                .collect(Collectors.toSet());
+        System.out.println(param + "을 구독한 유저 목록 : " + filteredUserNames);
+
+        if (onlineUserResponse.getUsers().isEmpty()) {
+            onlineUserResponse.setUsers(filteredUserNames);
+        } else {
+            setUsers(filteredUserNames, onlineUserResponse);
+        }
+    }
+
+    public void setUsers(Set<String> newUsers, OnlineUserResponse onlineUserResponse) {
+        Set<String> returnList = onlineUserResponse.getUsers();
+        returnList.addAll(newUsers);
+        onlineUserResponse.setUsers(returnList);
     }
 
 }
