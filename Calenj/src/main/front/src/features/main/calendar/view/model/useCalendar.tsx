3a5ebf72c1e537e063b8fbb91a91e3bdb3228e2f
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-06-13 20:56:21 +0900

    06.13 tag 저장 추가

--- Calenj/src/main/front/src/features/main/calendar/view/model/useCalendar.tsx
+++ Calenj/src/main/front/src/features/main/calendar/view/model/useCalendar.tsx
@@ -4,14 +4,12 @@ import {createEventId} from "../utils/event-utils";
 import {ReturnCalendar, ReturnExtendedProps} from "./types";
 import {Dictionary} from "@fullcalendar/core";
 import {RRule, Options, Weekday} from 'rrule';
+import axios from "axios";
 
 
 export const useCalendar = ():ReturnCalendar =>{
     const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
     type RepeatOption = '일' | '주' | '달' | '년';
-
-
-
     const weekArr = [RRule.SU,RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA]
 
     const freqHash = {
@@ -20,6 +18,10 @@ export const useCalendar = ():ReturnCalendar =>{
         달: RRule.MONTHLY,
         년: RRule.YEARLY
     };
+
+    useEffect(() => {
+        // axios.get("api/getUserSchedule")
+    }, []);
 
     //이벤트 변경시 api 처리
     const handleEvents = useCallback(
