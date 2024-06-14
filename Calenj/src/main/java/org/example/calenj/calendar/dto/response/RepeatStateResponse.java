612e4c3c4844304d36feee64ea7f3d7109558be7
kwontaein <zodls1128@gmail.com>
2024-06-14 16:23:07 +0900

    2024_06_14

--- Calenj/src/main/java/org/example/calenj/calendar/dto/response/RepeatStateResponse.java
+++ Calenj/src/main/java/org/example/calenj/calendar/dto/response/RepeatStateResponse.java
@@ -1,6 +1,5 @@
 package org.example.calenj.calendar.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.sql.Timestamp;
@@ -8,7 +7,6 @@ import java.util.Date;
 import java.util.UUID;
 
 @Data
-@AllArgsConstructor
 public class RepeatStateResponse {
     private UUID scheduleId;
 
@@ -28,6 +26,31 @@ public class RepeatStateResponse {
 
     private int repeatCount;
 
-    private String repeatWeek;
+    private boolean[] repeatWeek;
 
+    public RepeatStateResponse(UUID scheduleId, Timestamp startTime, Timestamp endTime, boolean repeat, String repeatOption, String repeatMode, String repeatDeadline, Date repeatEnd, int repeatCount, String repeatWeek) {
+        this.scheduleId = scheduleId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.repeat = repeat;
+        this.repeatOption = repeatOption;
+        this.repeatMode = repeatMode;
+        this.repeatDeadline = repeatDeadline;
+        this.repeatEnd = repeatEnd;
+        this.repeatCount = repeatCount;
+        this.repeatWeek = convertStringToArray(repeatWeek);
+    }
+
+    public static boolean[] convertStringToArray(String input) {
+        input = input.trim(); // 공백 제거
+        input = input.substring(1, input.length() - 1); // 대괄호 제거
+        String[] stringArray = input.split(",\\s*"); // 콤마와 공백을 기준으로 분할
+        boolean[] booleanArray = new boolean[stringArray.length]; // boolean 배열 초기화
+
+        for (int i = 0; i < stringArray.length; i++) {
+            booleanArray[i] = Boolean.parseBoolean(stringArray[i].trim()); // 문자열을 boolean 값으로 변환
+        }
+
+        return booleanArray; // boolean 배열 반환
+    }
 }
