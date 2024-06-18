8a3dd247c0c72b32fbc7992bdf90ffa97f9d3a0c
kwontaein <zodls1128@gmail.com>
2024-06-18 21:17:07 +0900

    2024_06_18

--- Calenj/src/main/front/src/features/main/calendar/view/ui/CalendarView.tsx
+++ Calenj/src/main/front/src/features/main/calendar/view/ui/CalendarView.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef, useState} from "react";
 import FullCalendar, {DateSelectArg, EventApi, EventInput} from "@fullcalendar/react";
 import {useCalendar} from "../model/useCalendar";
-import {GridCalendar_Container, StyledFullCalendar} from "./CalendarStyled";
+import {Draggable_Container, GridCalendar_Container, StyledFullCalendar} from "./CalendarStyled";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin, {DateClickArg} from "@fullcalendar/interaction";
@@ -16,20 +16,93 @@ import {AddDateEvent} from "../../create/";
 import {useFetchDateEventTag} from "../../../../../entities/reactQuery";
 import {CalendarEventView} from "./CalendarEventView";
 import {useCalendarController} from "../model/useCalendarController";
+import {boolean} from "yup";
+import {useComponentSize} from "../../../../../shared/model";
+import {Toggle_Container, Toggle_Item} from "../../../../../shared/ui/SharedStyled";
+import {ExternalEvents} from "../../stamp/ExternalEvents";
 
+// 이벤트 인터페이스 정의
+interface Event {
+    id: number;
+    title: string;
+    color: string;
+    custom?: string;
+}
+
+interface AppState {
+    weekendsVisible: boolean;
+    externalEvents: Event[];
+    calendarEvents: Event[];
+}
 
 export const CalendarView: React.FC = () => {
     const {data} = useFetchDateEventTag();
     const {currentEvents, handleEvents, handleEventClick} = useCalendar(data);
     const [addEvent, setAddEvent] = useState<boolean>(false);
     const [selectInfo, setSelectInfo] = useState<DateSelectArg | null>(null);
-    const {calendarRef, handleNavLinkDayClick} = useCalendarController();
+    const nodeRef = useRef(null);
+    const [position, setPosition] = useState({x: 20, y: 20});
+    const [contentRef, contentSize] = useComponentSize()
+    const [dragAble, setDragAble] = useState<boolean>(false);
+
     const onClose = () => {
         setAddEvent(false)
     }
 
+    const handleStop = (e: DraggableEvent, data: DraggableData) => {
+        const {x, y} = data;
+        // Calculate the closest corner
+        const closestX = x < contentSize.width / 2 ? 20 : contentSize.width - 170;
+        const closestY = y < contentSize.height / 2 ? 20 : contentSize.height - 170;
+
+        setPosition({x: closestX, y: closestY});
+    };
+
+    const [state, setState] = useState<AppState>({
+        weekendsVisible: true,
+        externalEvents: [
+            {id: 34432, title: "Art 1", color: "#0097a7", custom: "커스텀 내용"},
+            {id: 323232, title: "Art 2", color: "#f44336"},
+            {id: 1111, title: "Art 3", color: "#f57f17"},
+            {id: 432432, title: "Art 4", color: "#90a4ae"}
+        ],
+        calendarEvents: []
+    });
+
+    const onEventAdd = () => {
+        const newEvent: Event = {
+            id: Date.now(), // 고유 ID 생성을 위해 현재 시간의 타임스탬프 사용
+            title: "New Event",
+            color: "#02daff",
+            custom: "custom details"
+        };
+
+        setState(prevState => ({
+            ...prevState,
+            externalEvents: [...prevState.externalEvents, newEvent]
+        }));
+    };
+
     return (
         <>
+            <Draggable nodeRef={nodeRef}
+                       defaultPosition={{x: 20, y: 20}}
+                       position={position}
+                       onStop={handleStop}
+                       disabled={dragAble}>
+                <Draggable_Container ref={nodeRef}>
+                    <div style={{display: "flex", justifyContent: "space-between"}}>
+                        <div>수정</div>
+                        <Toggle_Container $isClick={dragAble}
+                                          onClick={() => {
+                                              setDragAble((prevState) => !prevState)
+                                          }}>
+                            <Toggle_Item $toggleState={dragAble}/>
+                        </Toggle_Container>
+                    </div>
+                    <ExternalEvents isAble={dragAble} events={state.externalEvents} onEventAdd={onEventAdd}/>
+                </Draggable_Container>
+            </Draggable>
             {data &&
                 <GridCalendar_Container>
                     {addEvent &&
