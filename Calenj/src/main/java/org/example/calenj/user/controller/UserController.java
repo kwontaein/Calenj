7bd2880093dea9bc014500d7add55b3f9f6cb373
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-06-23 13:08:25 +0900

    06.23 사용자 정보 가져오기

--- Calenj/src/main/java/org/example/calenj/user/controller/UserController.java
+++ Calenj/src/main/java/org/example/calenj/user/controller/UserController.java
@@ -59,7 +59,7 @@ public class UserController {
     /**
      * 로그인
      */
-    @PostMapping("/api/getUserInfo")
+    @PostMapping("/api/login")
     public ResponseEntity<String> login(@RequestBody UserRequest userRequest) {
         return userService.login(userRequest.getUserEmail(), userRequest.getUserPassword());
     }
