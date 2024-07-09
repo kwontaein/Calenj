a5a429707437375cd5f2382c4e60c72353cb0336
kwontaein <zodls1128@gmail.com>
2024-07-09 17:44:54 +0900

    2024_07_09 웹소켓 온라인 리스트 수정

--- Calenj/src/main/java/org/example/calenj/websocket/service/WebSocketService.java
+++ Calenj/src/main/java/org/example/calenj/websocket/service/WebSocketService.java
@@ -290,31 +290,49 @@ public class WebSocketService {
     public void personalEvent(Authentication authentication, ChatMessageRequest request) {
         UserEntity userEntity = returnUserEntity(authentication);
         String userId = String.valueOf(userEntity.getUserId());
-        ChatMessageResponse response = filterNullFields(request);
 
-        if (request.getMessage() != null && request.getMessage().contains("ONLINE")) {
+        if (request.getMessage() == null) {
+            return;
+        }
+
+        if (request.getMessage().contains("ONLINE")) {
             //내가 접속 시 -> 온라인 목록 불러옴 + 다른사람들한테 나 온라인이라고 알리기
-            sendOnlineStateFirstTime(userId, response);
-            sendOnlineState(userId, response);
-        } else if (request.getMessage() != null && request.getMessage().contains("OFFLINE")) {
-
+            sendOnlineStateFirstTime(userId, filterNullFields(request));
+            sendOnlineState(userId, filterNullFields(request));
+        } else if (request.getMessage().contains("OFFLINE")) {
             //끊을 시 -> 오프라인이라고만 알리기
-            sendOnlineState(userId, response);
+            sendOnlineState(userId, filterNullFields(request));
         }
     }
 
     public void sendOnlineStateFirstTime(String userId, ChatMessageResponse response) {
+        Set<String> friendList = new HashSet<>();
+
         //온라인 유저 정보 다시 반환
         for (String destination : getDestination(userId)) {
             //온라인 유저 정보 받아서
             Set<String> userList = getUsers(destination);
+            if (userList == null) {
+                userList = new HashSet<>();
+            }
             userList.add(userId);
+
             //반환정보에 담기
             response.setOnlineUserList(userList);
             response.setTarget(extractTopic(destination));
             response.setParam(extractUUID(destination));
-            template.convertAndSendToUser(String.valueOf(response.getUserId()), "/topic/" + extractUUID(destination) + "/" + extractTopic(destination), response);
+
+            if (extractTopic(destination).equals("friendMsg")) {
+                friendList.addAll(userList);
+            } else {
+                template.convertAndSendToUser(userId, destination, response);
+            }
         }
+
+        response.setOnlineUserList(friendList);
+        response.setParam(userId);
+        response.setTarget("friendMsg");
+        template.convertAndSendToUser(userId, "/topic/personalTopic/" + userId, response);
     }
 
     public void sendOnlineState(String userId, ChatMessageResponse response) {
