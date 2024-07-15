7d6acbd563d1cad51ba1dcad436e2e97f05a70df
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-07-15 19:08:20 +0900

    07.15 채팅 리팩토링

--- Calenj/src/main/front/src/features/messsage/messageScrollBox2/model/useMessageScroll.tsx
+++ Calenj/src/main/front/src/features/messsage/messageScrollBox2/model/useMessageScroll.tsx
@@ -1,170 +1,188 @@
-import {useEffect, useMemo, useRef, useState} from "react";
-import {debounce, throttleByAnimationFrame} from "../../../../shared/lib";
-import {endPointMap, scrollPointMap} from "../../../../entities/redux";
-import {requestFile} from "../../../../entities/redux";
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from '../../../../entities/redux'
+
 import {Message} from "../../../../entities/reactQuery";
+import {MutableRefObject, useCallback, useEffect, useMemo, useRef, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {endPointMap, requestFile, RootState, scrollPointMap} from "../../../../entities/redux";
+import axios from "axios";
+import {debounce, throttleByAnimationFrame} from "../../../../shared/lib";
+import {bool} from "yup";
 
+interface ReturnScroll{
+    containerRef:React.MutableRefObject<HTMLDivElement | null>,
+    messageRefs : React.MutableRefObject<{[key: string]: HTMLDivElement | null; }>,
+    scrollToMessage: (chatUUID: string, alignToBottom?: boolean) => void,
+}
 
-export const useMessageScroll = (param:string, messageList:Message[], isFetching:boolean) : React.MutableRefObject<HTMLDivElement|null> =>{
-    const scrollRef = useRef<HTMLDivElement | null>(null); //채팅스크롤 Ref
-    const [prevScrollHeight , setPrevScrollHeight] = useState<number|null>(null);//RELOAD 시 이전 높이를 저장하는데 사용
-    const beforeScrollTop = useRef<number>(); //이전 스크롤의 위치를 기억
-    const [socketEvent,setSocketEvent] = useState<boolean>(false); //내가 socketEvent를 발생시킨 여부 (식별하기 위해 사용)
+export const useMessageScroll = (connectMessages:Message[], firstPage:boolean, lastPage:boolean) :ReturnScroll =>{
+
     const dispatch = useDispatch();
-    const stomp = useSelector((state:RootState)=>state.stomp); // 리덕스 상태 구독
+    const stomp = useSelector((state:RootState)=>state.stomp)
     const {screenHeightSize,mode, clickState} = useSelector((state:RootState)=>state.subNavigation.group_subNavState)
     const {inputSize} = useSelector((state:RootState) => state.messageInputSize);
+    const stompParam = useSelector((state: RootState) => state.stomp.param)
     const beforeInputSize = useRef(inputSize);
-
-    const updateScroll_throttling = useMemo(() => {
-        if(socketEvent) setSocketEvent(false)
-        return throttleByAnimationFrame(() => {
-            updateScroll()
-        });
-    }, [param]);
-
-
+    const userId = localStorage.getItem("userId");
+    //높이 관련
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const messageRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+    const isRender = useRef<boolean>();
+    
     useEffect(() => {
-        return () => {
-            if(beforeScrollTop.current===undefined) return
-            //스크롤이 존재했는지 체크
-            scrollPointMap.set(param,beforeScrollTop.current);
-        }
-    }, [param]);
-
-
-    useEffect(() => {
-        if(!scrollRef.current || !beforeScrollTop.current) return
-        const {scrollTop, scrollHeight, clientHeight} = scrollRef.current
+        if(!containerRef.current) return
+        const {scrollTop, scrollHeight, clientHeight} = containerRef.current
         if(clientHeight + scrollTop===scrollHeight) { //맨아래에 위치해 있으면 위치조정 필요 X
             beforeInputSize.current = inputSize;//return 전 input 사이즈 저장
             return
         }
-        scrollRef.current.scrollTop -=(beforeInputSize.current- inputSize)
-        beforeScrollTop.current = scrollRef.current.scrollTop
+        containerRef.current.scrollTop -=(beforeInputSize.current- inputSize)
         beforeInputSize.current = inputSize;
 
     }, [inputSize]);
+    //맨 마지막 페이지인지
 
+    //아래로 내려가기 버튼
+    const [showDown, setShowDown] = useState<boolean>(true);
+    // //위로 내용 불러오기
+    // const [hasMoreTop, setHasMoreTop] = useState(true);
+    // //아래로 내용 불러오기
+    // const [hasMoreBottom, setHasMoreBottom] = useState(true);
 
 
 
     //컴포넌트 랜더링과 상관없이 인스턴스를 유지하게 memo
     const debouncing_EndPoint = useMemo(() => {
         return debounce(() => {
-            endPointMap.set(param, 0)
-            dispatch(requestFile({ target: 'groupMsg', param: param, requestFile: "ENDPOINT", nowLine: 0 }));
-            console.log('엔드포인트 갱신');
+            endPointMap.set(stompParam, 0)
+            dispatch(requestFile({ target: 'groupMsg', param: stompParam, requestFile: "ENDPOINT", nowLine: 0 }));
         }, 1000);
-    }, [param]);
+    }, [stompParam]);
 
     const updateEndpoint = () => {
         debouncing_EndPoint();
     }
 
-    const scrollToBottom = () => {
-        debouncing_EndPoint()
-        setTimeout(() => {
-            if (scrollRef.current) {
-                scrollRef.current.scrollTop = scrollRef.current.scrollHeight
-                beforeScrollTop.current = scrollRef.current.scrollTop
+
+    //높이 관련(엔드포인트 찍기 위한 div)
+    //높이 관련(메세지 uuid 위치로 이동)
+
+
+
+
+
+    // 스크롤 => chatUUID로 div를 조회하고 높이설정 => 메시지 fetchMoreMessages 이후 세팅
+    const scrollToMessage = useCallback((chatUUID: string, alignToBottom: boolean = false) => {
+        const messageDiv = messageRefs.current[chatUUID];
+        if (messageDiv && containerRef.current) {
+            if (alignToBottom) {
+                containerRef.current.scrollTop = messageDiv.offsetTop - containerRef.current.clientHeight + messageDiv.clientHeight;
+            } else {
+                containerRef.current.scrollTop = messageDiv.offsetTop - 80;
             }
-        }, 50)
-    };
+        }
+    }, []);
+
+
+    //스크롤 => 맨 밑으로 이동
+    const scrollToBottom = useCallback(() => {
+        if (containerRef.current) {
+            containerRef.current.scrollTop = containerRef.current.scrollHeight;
+            updateEndpoint()
+        }
+    }, []);
+
+
+
+    useEffect(() => {
+        if(clickState==='' || !containerRef.current) return;
+        containerRef.current.scrollTop += (clickState!=="" && mode==="column"? Math.round(screenHeightSize) : 0);
+    }, [clickState]);
+
+
+
+    const updateScroll = () => {
+        if (!containerRef.current) return
+        const {scrollTop, scrollHeight, clientHeight} = containerRef.current;
+        const sendUser = stomp.receiveMessage.userId
+        if(endPointMap.get(stompParam) ===0) return;
+
+        //스크롤에 따른 표시
+        if (scrollHeight > clientHeight && scrollTop + clientHeight === scrollHeight){
+            scrollToBottom();
+        }
+        if(userId ===sendUser){
+            scrollToBottom();
+        }
+    }
+
+
+
+
+    const updateScroll_throttling = useMemo(() => {
+        return throttleByAnimationFrame(() => {
+            updateScroll()
+        });
+    }, [stompParam]);
 
 
     const addScrollEvent=()=>{
         //isLoading이 falset가 돼야 스크롤 scrollRef가 잡혀서 셋팅됨
         //로딩된 이후엔 스크롤을 안 내려야함
-        if (!scrollRef.current) return
-        scrollRef.current.addEventListener('scroll', updateScroll_throttling);
+        if (!containerRef.current || connectMessages.length===0) return
+        containerRef.current.addEventListener('scroll', updateScroll_throttling);
         //메시지가 없으면 스크롤 세팅필요 X
-        if(messageList.length===0) return;
-        //infiniteQuery 첫세팅 시에만 체크됨 => scrollPointMap이 등록되지 않은상황, 리패칭 시 스크롤세팅
-        if(scrollPointMap.get(param) === undefined){
-            if (endPointMap.get(param) === 0){
-                scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-                scrollPointMap.set(param, scrollRef.current.scrollHeight)
-            }else{
-                const scrollDiv = scrollRef.current;
-                const targetElement = scrollDiv.querySelector('.엔드포인트')
-                if (targetElement) {
-                    const {bottom} = targetElement.getBoundingClientRect();
 
-                    scrollRef.current.scrollTop = bottom - 300;
-                    scrollPointMap.set(param, bottom - 300)
-                }
+        if(!isRender.current){ //첫 랜더링에만 적용
+            const hasEndPoint = connectMessages.some((message: Message) => message.chatUUID === "엔드포인트");
+            if (hasEndPoint) {
+                scrollToMessage("엔드포인트");
+            } else {
+                scrollToBottom();
             }
+            isRender.current =true;
         }
-        //저장된 스크롤 포인트가 있으면 세팅
-        else {
-            scrollRef.current.scrollTop = (scrollPointMap.get(param) + (clickState!=="" && mode==="column"? Math.round(screenHeightSize) : 0)); //반올림돼서 올라가는 것으로 추측 +1 로 오차 줄이기
-        }
-
-        const {scrollHeight, clientHeight} = scrollRef.current;
-        //메시지가 와있는데 스크롤이 없다면 앤드포인트 갱신
-        if(endPointMap.get(param) !== 0 && scrollHeight === clientHeight){
-            updateEndpoint();
-        }
+        
     }
 
 
-    //stomp 감지하여 소켓 이벤트 활성화 => updateScroll이후 다시 비활성화 => 불필요한 스크롤 업데이트 방지
-    useEffect(() => {
-        setSocketEvent(true);
-        const {state} = stomp.receiveMessage;
-        if (param !== stomp.receiveMessage.param || state!=="SEND") return
-        updateScroll()
-    }, [stomp.receiveMessage]);
-
-    //스크롤 상태에 따른 endPoint업데이트
-    const updateScroll = () => {
-        if (!scrollRef.current) return
-        const {scrollTop, scrollHeight, clientHeight} = scrollRef.current;
-        const {userId} = stomp.receiveMessage
-        beforeScrollTop.current = scrollTop - (clickState!=="" && mode==="column"? Math.round(screenHeightSize) : 0);
-
-
-        if(userId === localStorage.getItem('userId') && socketEvent){
-            scrollToBottom();
-        }
-        if(endPointMap.get(param) ===0) return;
-
-        //스크롤이 존재하지 않으면
-        if(scrollHeight === clientHeight){
-            updateEndpoint();
-        }else if (scrollHeight > clientHeight && scrollTop + clientHeight === scrollHeight){
-            scrollToBottom();
-        }
-
-    }
-
-    //메시지 파일 요청 시 nowLine을 업데이트 > 현재 scrollTop 저장
-    useEffect(() => {
-        const {requestFile,nowLine} = stomp
-        if(!scrollRef.current || requestFile !=="RELOAD") return
-        setPrevScrollHeight(scrollRef.current.scrollHeight)
-    }, [stomp.nowLine]);
 
 
 
     useEffect(()=>{
-        if(messageList.length===0) return
+        const {param, state} = stomp.receiveMessage
+        if(stompParam !== param || state!=='SEND' || connectMessages.length === 0 || !containerRef.current) return
+        const {scrollTop, scrollHeight, clientHeight} = containerRef.current;
+
+        if (scrollHeight > clientHeight && scrollTop + clientHeight === scrollHeight) {
+            scrollToBottom();
+        } else {
+            setShowDown(true);
+        }
+    },[stomp.receiveMessage.receivedUUID])
+
+
+    //데이터 => 아래로 버튼 클릭시 상호작용
+    const goBottom = async () => {
+        if (lastPage) { //마지막 페이지이고, 80개가 넘지 않으면 다시 불러올 필요 없음.
+            scrollToBottom();
+        } else {
+            // getInitialMessages();
+        }
+    }
+
+    //메세지 추가 로딩
+    useEffect(() => {
+        //스크롤 조정
+        if(connectMessages.length===0){
+            isRender.current = false;
+        }
         addScrollEvent()
-
-        const {state} =stomp.receiveMessage
-        if(state!=="RELOAD") return
-        // //저장한 이전높이인 prev만큼 빼줌
-        if(!scrollRef.current || !prevScrollHeight) return;
-        scrollRef.current.scrollTop = scrollRef.current.scrollHeight - prevScrollHeight
-        scrollPointMap.set(param,scrollRef.current.scrollTop);
-        setPrevScrollHeight(null)
-
-    },[messageList])
-    //원하는 타이밍에 정확히 세팅하려면 메시지가 세팅이 완료된 후 함수를 호출해야함
+        return () => {
+            if (containerRef.current) {
+                containerRef.current.removeEventListener('scroll',  updateScroll_throttling);
+            }
+        };
+    }, [connectMessages]);
 
 
-    return scrollRef
+    return {containerRef, messageRefs, scrollToMessage};
 }
--- Calenj/src/main/front/src/features/messsage/messageScrollBox2/ui/MessageScrollBox.tsx
+++ Calenj/src/main/front/src/features/messsage/messageScrollBox2/ui/MessageScrollBox.tsx
@@ -1,4 +1,3 @@
-import {useMessageScroll} from "../../index";
 import {useMessageData} from "../model/useMessageData";
 import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
 import {
@@ -9,15 +8,10 @@ import {
     shortAHMFormat,
     throttleByAnimationFrame
 } from "../../../../shared/lib";
-import {useComponentSize, useIntersect} from "../../../../shared/model";
 import {useDispatch, useSelector} from "react-redux";
 import {
-    endPointMap,
     requestFile,
     RootState,
-    scrollPointMap,
-    sendStompMsg,
-    updateAppPosition
 } from "../../../../entities/redux";
 import {
     DateContainer,
@@ -31,245 +25,49 @@ import {RowFlexBox} from "../../../../shared/ui/SharedStyled";
 import {Message} from "../../../../entities/reactQuery"
 import {dateOperation} from "../lib/dateOperation";
 import axios from "axios";
+import {useIntersect} from "../../../../shared/model";
+import {useMessageScroll} from "../model/useMessageScroll";
 
 export const MessageScrollBox2: React.FC = () => {
     const {inputSize} = useSelector((state: RootState) => state.messageInputSize);
     const {userNameRegister} = useSelector((state: RootState) => state.userNameRegister);
-
-    const dispatch = useDispatch();
-    const stomp = useSelector((state: RootState) => state.stomp);
-
-    const {compareDate} = useMessageData(stomp.param)
+    const {connectMessages, firstPage, lastPage, fetchMoreMessages, compareDate} = useMessageData()
+    const {containerRef,messageRefs, scrollToMessage} = useMessageScroll(connectMessages, firstPage,lastPage)
 
 
-    //메세지목록
-    const [messages, setMessages] = useState<Message[]>([]);
-    //웹소켓 메시지 목록
-    const [newMessages, setNewMessages] = useState<Message[]>([]);
-    //마지막 페이지인지 구분하기 위한 값
-    const [lastMessage, setLastMessage] = useState<Message>();
-
-    //맨 처음 페이지인지
-    const [firstPage, setIsFirstPage] = useState<boolean>(false);
-    //맨 마지막 페이지인지
-    const [lastPage, setIsLastPage] = useState<boolean>(true);
-
-    //아래로 내려가기 버튼
-    const [showDown, setShowDown] = useState<boolean>(true);
-
-    //위로 내용 불러오기
-    const [hasMoreTop, setHasMoreTop] = useState(true);
-    //아래로 내용 불러오기
-    const [hasMoreBottom, setHasMoreBottom] = useState(true);
-
-    //보낼 채팅 내용
-    const [inputMessage, setInputMessage] = useState("");
-
-    //api 중복 호출 방지
-    const isFetchingRef = useRef(false); // 요청 진행 여부를 추적하는 ref
-    const isSendRef = useRef(false); // 요청 진행 여부를 추적하는 ref
-
-    //높이 관련
-    const containerRef = useRef<HTMLDivElement | null>(null);
-    const topDivRef = useRef<HTMLDivElement | null>(null);
-    const bottomDivRef = useRef<HTMLDivElement | null>(null);
-
-
-    //높이 관련(엔드포인트 찍기 위한 div)
-    const endPointRef = useRef<HTMLDivElement | null>(null);
-    //높이 관련(메세지 uuid 위치로 이동)
-    const messageRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
-
-    const stompParam = useSelector((state: RootState) => state.stomp.param)
-    const userId = localStorage.getItem("userId");
-
-    //웹소켓 메세지 받기
-    useEffect(() => {
-        const {state, param, message} = stomp.receiveMessage;
-        if (stompParam !== param || messages.length === 0) return;
-        if (message && state === "SEND") {
-            setNewMessages([...newMessages, ...message]);
-            setLastMessage(message[message.length - 1]);
-        }
-    }, [stomp.receiveMessage.receivedUUID]);
-
-
-    const scrollToMessage = useCallback((chatUUID: string, alignToBottom: boolean = false) => {
-        const messageDiv = messageRefs.current[chatUUID];
-        if (messageDiv && containerRef.current) {
-            if (alignToBottom) {
-                containerRef.current.scrollTop = messageDiv.offsetTop - containerRef.current.clientHeight + messageDiv.clientHeight;
-            } else {
-                containerRef.current.scrollTop = messageDiv.offsetTop - 80;
-            }
-        }
-    }, []);
-
-    //스크롤 맨 밑으로 이동
-    const scrollToBottom = useCallback(() => {
-        if (containerRef.current) {
-            containerRef.current.scrollTop = containerRef.current.scrollHeight;
-        }
-    }, []);
-
-    //시작하자마자 데이터 받아오기
-    useEffect(() => {
-        fetchInitialMessages();
-    }, []);
-
-    //시작하자마자 데이터 받아오기 #2
-    const fetchInitialMessages = async () => {
-        const response = await axios.post('/api/getChattingFile', {
-            param: stompParam,
-            userId: userId,
-        });
-        setMessages(response.data);
-        setLastMessage(response.data[response.data.length - 1])
-        setIsLastPage(true);
-
-        if (response.data[0].chatUUID === "시작라인") {
-            setIsFirstPage(true);
-        }
-        setTimeout(() => {
-            // 엔드포인트 메시지가 있는지 확인
-            const hasEndPoint = response.data.some((message: Message) => message.chatUUID === "엔드포인트");
-            if (hasEndPoint) {
-                scrollToMessage("엔드포인트");
-            } else {
-                scrollToBottom();
-            }
-        }, 0);
-    };
-
-    useEffect(() => {
-        if (newMessages.length > 0) {
-            if (containerRef.current) {
-                const {scrollTop, scrollHeight, clientHeight} = containerRef.current;
-                const isAtBottom = scrollHeight - scrollTop <= clientHeight + 300; // 바닥에서 50px 이내에 있을 경우
-                if (isAtBottom) {
-                    scrollToBottom();
-                } else {
-                    setShowDown(true);
+    const topRef = useIntersect((entry, observer) => {
+        if (!firstPage && connectMessages.length>0) {
+            observer.unobserve(entry.target);
+            fetchMoreMessages('older', connectMessages[0]?.chatUUID).then((chatUUID)=>{
+                if(chatUUID){
+                    scrollToMessage(chatUUID)
                 }
-            }
+            })
         }
-    }, [newMessages, scrollToBottom]);
-
-    //아래로 버튼 클릭시 상호작용
-    const goBottom = async () => {
-        if (lastPage) { //마지막 페이지이고, 80개가 넘지 않으면 다시 불러올 필요 없음.
-            scrollToBottom();
-        } else {
-            fetchInitialMessages();
+    });
+    const bottomRef = useIntersect((entry, observer) => {
+        if (!firstPage && connectMessages.length>0) {
+            observer.unobserve(entry.target);
+            fetchMoreMessages('newer', connectMessages.at(-1)?.chatUUID).then((chatUUID)=>{
+                if(chatUUID){
+                    scrollToMessage(chatUUID)
+                }
+            })
         }
-    }
-
-    //메세지 추가 로딩
-    useEffect(() => {
-        //스크롤 조정
-        const handleScroll = () => {
-            if (!containerRef.current) return;
-            const container = containerRef.current;
-
-            const topDivRect = topDivRef.current?.getBoundingClientRect();
-            const bottomDivRect = bottomDivRef.current?.getBoundingClientRect();
-            const endPointDivRect = endPointRef.current?.getBoundingClientRect();
-            const containerRect = container.getBoundingClientRect();
-
-            if (topDivRect && topDivRect.bottom >= containerRect.top && hasMoreTop) {
-                fetchMoreMessages('older', messages[0]?.chatUUID).then(r => console.log());
-            }
-            if (bottomDivRect && bottomDivRect.top <= containerRect.bottom && hasMoreBottom) {
-                fetchMoreMessages('newer', messages[messages.length - 1]?.chatUUID).then(r => console.log());
-            }
-            if (lastPage && endPointDivRect && endPointDivRect.top <= containerRect.bottom) {
-                debouncing_EndPoint(); // lastPage가 true이고 화면이 맨 아래에 닿았을 때 함수 호출
-            }
-            //맨아래 ,
-            const {scrollHeight, clientHeight, scrollTop} = containerRef.current;
-            if (scrollHeight === clientHeight + scrollTop && lastPage) {
-                debouncing_EndPoint();
-            }
-        };
-
-        if (containerRef.current) {
-            containerRef.current.addEventListener('scroll', handleScroll);
-        }
-        return () => {
-            if (containerRef.current) {
-                containerRef.current.removeEventListener('scroll', handleScroll);
-            }
-        };
-    }, [hasMoreTop, hasMoreBottom, messages]);
+    });
 
 
-    //추가 데이터 받기 및 가공
-    const fetchMoreMessages = async (position: 'older' | 'newer', chatUUID?: string) => {
-        try {
-            if (!chatUUID || isFetchingRef.current) return; // 메시지가 없거나 요청 진행 중이면 반환
-            isFetchingRef.current = true; // 요청 시작 플래그 설정
 
-            setTimeout(() => {
-                isFetchingRef.current = false; // 3초 후 요청 가능하게 설정
-            }, 500);
 
-            const response = await axios.post('/api/ReloadChattingFile', {
-                param: stompParam,
-                userId: userId,
-                chatId: chatUUID,
-                newOrOld: position
-            });
-
-            setMessages(prevMessages => {
-                if (response.data[0].chatUUID === prevMessages[0]?.chatUUID) {
-                    return prevMessages; // 동일한 메시지의 중복 방지
-                }
-                const newMessages = position === 'older'
-                    ? [...response.data, ...prevMessages] :
-                    [...prevMessages, ...response.data];
-
-                if (newMessages[0].chatUUID === "시작라인") {
-                    setIsFirstPage(true);
-                }
-                if (newMessages[newMessages.length - 1].chatUUID === lastMessage?.chatUUID) {
-                    setNewMessages([]);
-                    setIsLastPage(true);
-                }
-
-                if (newMessages.length >= 120) {
-                    if (position === 'older') {
-                        setIsLastPage(false);
-                        return newMessages.slice(0, 60);
-                    } else {
-                        setIsFirstPage(false);
-                        return newMessages.slice(newMessages.length - 60);
-                    }
-                }
-                return newMessages;
-            });
-
-            setTimeout(() => {
-                scrollToMessage(chatUUID, position === 'newer');
-            }, 0);
-
-        } catch (error) {
-            console.error(`Error fetching ${position} messages:`, error);
-        }
-    };
-
-    const debouncing_EndPoint = useMemo(() => {
-        return debounce(() => {
-            dispatch(requestFile({target: 'groupMsg', param: stompParam, requestFile: "ENDPOINT", nowLine: 0}));
-        }, 1000);
-    }, [stompParam]);
 
     const MessageBox = useMemo(() => {
 
-        const connectList = lastPage ? [...messages, ...newMessages] : [...messages];
+        const connectList =connectMessages.filter((message:Message)=> message.chatUUID !=="시작라인");
         return (
             <ScrollableDiv ref={containerRef}>
-                {!firstPage && <div className="scrollTop" ref={topDivRef}></div>}
-                {connectList.map((message: Message, index: number) => (
+                {!firstPage && <div className="scrollTop" ref={topRef}></div>}
+                {connectList.length >0 &&
+                    ( connectList.map((message: Message, index: number) => (
                     <div key={message.chatUUID}>
                         {(index !== 0 && compareDate(connectList[index - 1].sendDate, message.sendDate) && (message.chatUUID !== '엔드포인트')) &&
                             <HR_NewDate
@@ -325,19 +123,18 @@ export const MessageScrollBox2: React.FC = () => {
                             }
                         </MessageBoxContainer>
                     </div>)
-                )}
-                {!lastPage ?
-                    <div className="scrollBottom" style={{marginTop: '10px'}} ref={bottomDivRef}></div> :
-                    <div className="scrollBottom" style={{marginTop: '10px'}} ref={endPointRef}></div>
-                }
-                <button onClick={goBottom}
+                ))}
+
+                <div className="scrollBottom" style={{marginTop: '10px'}} ref={bottomRef}></div> :
+
+                <button
                         style={{position: 'fixed', bottom: '70px', right: '10px', backgroundColor: "grey"}}>
                     <p>맨 아래로</p>
                 </button>
 
             </ScrollableDiv>
         );
-    }, [messages, newMessages]);
+    }, [connectMessages]);
 
 
     return (
