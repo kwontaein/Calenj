5c13506bd9a366a908ba9bfc66476b1775b94c86
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-07-26 21:56:05 +0900

    07.26 채팅 캐싱

--- Calenj/src/main/front/src/features/messsage/messageScrollBox/model/useMessageData.tsx
+++ Calenj/src/main/front/src/features/messsage/messageScrollBox/model/useMessageData.tsx
@@ -1,16 +1,27 @@
 import {changeDateForm} from "../../../../shared/lib";
 import axios from "axios";
 import {MutableRefObject, useCallback, useEffect, useMemo, useRef, useState} from "react";
-import {Message} from "../../../../entities/reactQuery";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../../entities/redux";
+import {Message, QUERY_CHATTING_KEY, useChatFileInfinite} from "../../../../entities/reactQuery";
+import {connect, useSelector} from "react-redux";
+import {endPointMap, RootState} from "../../../../entities/redux";
 import {useIntersect} from "../../../../shared/model";
+import {FetchData} from "../../../../entities/reactQuery/model/types";
+import {
+    FetchPreviousPageOptions,
+    InfiniteData,
+    InfiniteQueryObserverResult,
+    useInfiniteQuery, useQueryClient
+} from "@tanstack/react-query";
+import {all} from "@redux-saga/core/effects";
 
 interface useMessageData {
-    connectMessages: Message[],
-    firstPage: boolean,
-    lastPage: boolean,
-    fetchMoreMessages: (position: 'older' | 'newer', chatUUID?: string) => Promise<string | void>,
+    messageList: Message[],
+    chatUUID:string,
+    position:string,
+    topRef: React.RefObject<HTMLDivElement>,
+    bottomRef: React.RefObject<HTMLDivElement>,
+    hasNextPage:boolean,
+    hasPreviousPage:boolean
     compareDate: (date1: string, date2: string) => boolean,
 }
 
@@ -20,30 +31,31 @@ export const useMessageData = (): useMessageData => {
     const stompParam = useSelector((state: RootState) => state.stomp.param)
     const userId = localStorage.getItem("userId");
     //메세지목록
-    const [messages, setMessages] = useState<Message[]>([]);
     //웹소켓 메시지 목록
     const [newMessages, setNewMessages] = useState<Message[]>([]);
+    const [chatUUID, setChatUUID] = useState<string>('');
+    const [position, setPosition] = useState<string>('older');
     //마지막 페이지인지 구분하기 위한 값
     const [lastMessage, setLastMessage] = useState<Message>();
+    const queryClient = useQueryClient();
 
-    //맨 처음 페이지인지
-    const [firstPage, setIsFirstPage] = useState<boolean>(false);
-    //맨 마지막 페이지인지
-    const [lastPage, setIsLastPage] = useState<boolean>(true);
-    //api 중복 호출 방지
-    const [isFetching, setIsFetching] = useState(false); // 요청 진행 여부를 추적하는 ref
+    const {data, isFetching, hasNextPage, hasPreviousPage, fetchNextPage, fetchPreviousPage} = useChatFileInfinite(stompParam,userId||'')
+
+    const messageList = useMemo(() => {
+        if (data && !isFetching) {
+            const messages = data.pages.reduce((prev, current) => prev.concat(current)).filter((message)=>message.chatUUID!=="시작라인")
+            return hasNextPage ? messages : [...messages, ...newMessages]
+        }
+        return [];
+    }, [data, isFetching])
 
 
-    //시작하자마자 데이터 받아오기
-    useEffect(() => {
-        getInitialMessages();
-    }, []);
 
 
     //데이터 => 웹소켓 새로운 메세지 받기
     useEffect(() => {
         const {state, param, message} = stomp.receiveMessage;
-        if (stompParam !== param || messages.length === 0) return;
+        if (stompParam !== param ) return;
         if (message && state === "SEND") {
             setNewMessages([...newMessages, ...message]);
             setLastMessage(message[message.length - 1]);
@@ -52,80 +64,42 @@ export const useMessageData = (): useMessageData => {
 
 
 
-    const connectMessages = useMemo(() => {
-        if (messages.length === 0) return []
-        if (messages[0].chatUUID === "시작라인") {
-            setIsFirstPage(true);
-        } else {
-            setIsFirstPage(false)
+    const topRef = useIntersect((entry, observer) => {
+        if (hasPreviousPage && messageList.length > 0) {
+            observer.unobserve(entry.target);
+            setPosition("older")
+            setChatUUID(messageList[0].chatUUID)
+            if(!data) return
+            console.log(data.pageParams)
+            if(data.pages.length>3) {
+                queryClient.setQueryData([QUERY_CHATTING_KEY, stompParam], (data: InfiniteData<(Message | null)[], unknown> | undefined) => ({
+                    pages: data?.pages.slice(0, 3),
+                    pageParams: data?.pageParams.slice(0, 3)
+                }));
+            }
+            fetchPreviousPage()
+
         }
-        if ([...messages, ...newMessages].at(-1)?.chatUUID === lastMessage?.chatUUID) {
-            setIsLastPage(true)
-        } else {
-            setIsLastPage(false)
+    });
+
+    const bottomRef = useIntersect((entry, observer) => {
+        if (hasNextPage && messageList.length > 0) {
+            console.log("실행")
+            observer.unobserve(entry.target);
+            setPosition("newer")
+            setChatUUID(messageList[messageList.length - 1].chatUUID)
+            if (!data) return
+            console.log(data.pageParams)
+            if (data.pages.length > 3) {
+                queryClient.setQueryData([QUERY_CHATTING_KEY, stompParam], (data: InfiniteData<(Message | null)[], unknown> | undefined) => ({
+                    pages: data?.pages.slice(1, 4),
+                    pageParams: data?.pageParams.slice(1, 4)
+                }));
+            }
+            fetchNextPage()
         }
 
-        return (lastPage ? [...messages, ...newMessages] : [...messages]).filter((message: Message) => message.chatUUID !== "시작라인")
-    }, [messages, newMessages])
-
-
-    //데이터 => 초기 Message get
-    const getInitialMessages = async () => {
-        const response = await axios.post('/api/getChattingFile', {
-            param: stompParam,
-            userId: userId,
-        });
-        setMessages(response.data);
-        setLastMessage(response.data.at(-1))
-        setIsLastPage(true);
-    };
-
-
-    useEffect(() => {
-        if (!isFetching) return
-        setTimeout(() => {
-            setIsFetching(false);
-        }, 500)
-    }, [connectMessages]);
-
-    //데이터 => 추가 데이터 받기 및 가공
-    const fetchMoreMessages = async (position: 'older' | 'newer', chatUUID?: string): Promise<string | void> => {
-        if (!chatUUID || isFetching) return; // 메시지가 없거나 요청 진행 중이면 반환
-        setIsFetching(true) // 요청 시작 플래그 설정
-
-        axios.post('/api/ReloadChattingFile', {
-            param: stompParam,
-            userId: userId,
-            chatId: chatUUID,
-            newOrOld: position
-        }).then((response) => {
-            setMessages(prevMessages => {
-                if (!response.data) return prevMessages
-                if (response.data[0].chatUUID === prevMessages[0]?.chatUUID) {
-                    return prevMessages; // 동일한 메시지의 중복 방지
-                }
-
-                if (position === 'older') {
-                    if ((response.data.length + prevMessages.length) >= 120) {
-                        return [...response.data, ...prevMessages].slice(0, 60);
-                    } else {
-                        return [...response.data, ...prevMessages]
-                    }
-                } else { //position => newer
-                    if ((response.data.length + prevMessages.length) >= 120) {
-                        return [...prevMessages, ...response.data].slice(prevMessages.length + response.data.length - 62);
-                    } else {
-                        return [...prevMessages, ...response.data]
-                    }
-                }
-            });
-
-        }).catch((err) => {
-            console.error(`Error fetching ${position} messages:`, err);
-        });
-
-        return chatUUID;
-    };
+    });
 
 
     //--------------------------------------------------------------------------------------------------------------- 의존성을 활용한 페이지 랜더링 및 업데이트 관리
@@ -134,5 +108,5 @@ export const useMessageData = (): useMessageData => {
         if (changeDateForm(date1).getMonth() !== changeDateForm(date2).getMonth()) return true
         return changeDateForm(date1).getFullYear() !== changeDateForm(date2).getFullYear();
     }
-    return {connectMessages, firstPage, lastPage, fetchMoreMessages, compareDate};
+    return {messageList, chatUUID, position, topRef, bottomRef, hasNextPage, hasPreviousPage, compareDate};
 }
