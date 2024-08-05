420cd558a5b1bfb099d02a71688c918e03a3f2f7
kwontaein <zodls1128@gmail.com>
2024-08-05 21:52:09 +0900

    08_05 #5

--- Calenj/src/main/front/src/features/group/event/detail/view/ui/ScheduleDetail.tsx
+++ Calenj/src/main/front/src/features/group/event/detail/view/ui/ScheduleDetail.tsx
@@ -49,84 +49,102 @@ export const ScheduleDetail: React.FC<GroupScheduleProps> = ({scheduleDetail}) =
     const {scheduleId, mapModal} = useSelector((state: RootState) => state.groupSchedule)
     const {userNameRegister} = useSelector((state: RootState) => state.userNameRegister)
 
-    const [optionState, setOptionState] = useReducer((prev)=>!prev, false)
-    const selectBox= useClickOutSideCheck(optionState, setOptionState)  // 스케줄 옵션관리
+    const [optionState, setOptionState] = useReducer((prev) => !prev, false)
+    const selectBox = useClickOutSideCheck(optionState, setOptionState)  // 스케줄 옵션관리
     const [showMember, setShowMember] = useReducer((prev) => !prev, false)
     const showMemberRef = useClickOutSideCheck(showMember, setShowMember) // 멤버 옵션관리
 
     const [month, setMonth] = useState<number>(new Date(scheduleDetail.startDate).getMonth())
     //수정관련
-    const {editMode,setEditMode, groupSchedule,dispatchGroupSchedule, mapHandler} = useGroupScheduleEdit(scheduleDetail)
-
+    const {
+        editMode,
+        setEditMode,
+        groupSchedule,
+        dispatchGroupSchedule,
+        mapHandler
+    } = useGroupScheduleEdit(scheduleDetail)
 
 
     const groupSubScheduleList = useFetchGroupSubScheduleList(scheduleId); //이 리스트로 넣으면 됨
-    const [subScheduleEdit,dispatchSubSchedule] = useReducer(groupSubScheduleReducer, groupSubScheduleList.data||[])
+    const [subScheduleEdit, dispatchSubSchedule] = useReducer(groupSubScheduleReducer, groupSubScheduleList.data || [])
     const useSubSchedule = useListDrag(subScheduleEdit, dispatchSubSchedule, groupSchedule.startDate)
+
 
     useEffect(() => {
         console.log(groupSubScheduleList.data)
     }, [groupSubScheduleList.data]);
 
-    const saveGroupSchedule = ()=>{
-        const postAble =subScheduleEdit.every((subSchedule)=> subSchedule.subScheduleTitle!=="")
-        if(postAble){
+    const saveGroupSchedule = () => {
+        const postAble = subScheduleEdit.every((subSchedule) => subSchedule.subScheduleTitle !== "")
+        if (postAble) {
             const postScheduleData = {
                 ...scheduleDetail,
                 ...groupSchedule,
-                groupSubSchedules : subScheduleEdit
+                groupSubSchedules: subScheduleEdit
             }
             saveSubScheduleApi(postScheduleData)
-                .then(()=>{
+                .then(() => {
                     window.alert('저장이 완료되었습니다')
                     groupScheduleList.refetch()
                     groupSubScheduleList.refetch()
                 })
-                .catch((err)=>{console.log('fxxk')})
-        }else{
+                .catch((err) => {
+                    console.log('fxxk')
+                })
+        } else {
             window.alert('일정의 제목을 입력해주세요.')
         }
     }
 
+    const mapElement = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        const map = new naver.maps.Map('map', {
+            center: new naver.maps.LatLng(37.3595704, 127.105399),
+            zoom: 10
+        });
+    })
 
     return (
         <ScheduleDetail_Container>
             <ScheduleDetailScroll_Wrapper $editMode={editMode}>
                 <ScheduleStartDate_Container>
                     <div>
-                    일정 시작 :
-                    {editMode ?
-                        <EventDatePicker
-                            dateFormat={'yy년 MM월 dd일 (EEE) aa hh시 mm분'} // 날짜 형태
-                            shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
-                            selected={groupSchedule.startDate}
-                            showTimeSelect={true}
-                            timeFormat={"HH:mm"}
-                            timeIntervals={30}
-                            onChange={(date: Date) => dispatchGroupSchedule({type: 'SET_START_DATE', payload: date})}
-                            locale={ko}
-                            minDate={scheduleDetail.startDate} // minDate 이전 날짜 선택 불가
-                            popperPlacement="right-start"
-                            timeClassName={() => "date-picker-time"}
-                            onMonthChange={(date: Date) => setMonth(date.getMonth())}
-                            dayClassName={(d) =>
-                                (d.getDate() === groupSchedule.startDate.getDate() && d.getMonth() === groupSchedule.startDate.getMonth())
-                                    ? 'custom-day selected-day'
-                                    : d.getMonth() === month
-                                        ? 'custom-day'
-                                        : 'custom-day gray-day'
-                            }
-                            $width={230}
-                            $height={20}
-                            $fontSize={15}
-                            $marginInline={0}
-                        />
-                    : ` ${AHMFormatV3(groupSchedule.startDate)}`}
+                        일정 시작 :
+                        {editMode ?
+                            <EventDatePicker
+                                dateFormat={'yy년 MM월 dd일 (EEE) aa hh시 mm분'} // 날짜 형태
+                                shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
+                                selected={groupSchedule.startDate}
+                                showTimeSelect={true}
+                                timeFormat={"HH:mm"}
+                                timeIntervals={30}
+                                onChange={(date: Date) => dispatchGroupSchedule({
+                                    type: 'SET_START_DATE',
+                                    payload: date
+                                })}
+                                locale={ko}
+                                minDate={scheduleDetail.startDate} // minDate 이전 날짜 선택 불가
+                                popperPlacement="right-start"
+                                timeClassName={() => "date-picker-time"}
+                                onMonthChange={(date: Date) => setMonth(date.getMonth())}
+                                dayClassName={(d) =>
+                                    (d.getDate() === groupSchedule.startDate.getDate() && d.getMonth() === groupSchedule.startDate.getMonth())
+                                        ? 'custom-day selected-day'
+                                        : d.getMonth() === month
+                                            ? 'custom-day'
+                                            : 'custom-day gray-day'
+                                }
+                                $width={230}
+                                $height={20}
+                                $fontSize={15}
+                                $marginInline={0}
+                            />
+                            : ` ${AHMFormatV3(groupSchedule.startDate)}`}
                     </div>
                     <ScheduleDotsIcon_Container onClick={setOptionState} ref={selectBox}>
                         <i className="bi bi-three-dots"/>
                         {optionState &&
-                            <Option_Container style={{marginLeft:'-90px'}}>
+                            <Option_Container style={{marginLeft: '-90px'}}>
                                 <Option_Item>
                                     <OptionIcon_Wrapper>
                                         <i className="bi bi-person-fill-gear"></i>
@@ -139,11 +157,11 @@ export const ScheduleDetail: React.FC<GroupScheduleProps> = ({scheduleDetail}) =
                                     </OptionIcon_Wrapper>
                                     권한관리
                                 </Option_Item>
-                                <Option_Item onClick={()=> setEditMode()} $isClick={editMode}>
+                                <Option_Item onClick={() => setEditMode()} $isClick={editMode}>
                                     <OptionIcon_Wrapper>
                                         <i className="bi bi-pencil-square"></i>
                                     </OptionIcon_Wrapper>
-                                    {editMode ? "수정완료" :"수정하기"}
+                                    {editMode ? "수정완료" : "수정하기"}
                                 </Option_Item>
                                 <Option_Item>
                                     <OptionIcon_Wrapper>
@@ -154,12 +172,12 @@ export const ScheduleDetail: React.FC<GroupScheduleProps> = ({scheduleDetail}) =
                             </Option_Container>
                         }
                     </ScheduleDotsIcon_Container>
-            </ScheduleStartDate_Container>
+                </ScheduleStartDate_Container>
                 <SchedulePrivacy_Container>
                     <span>공개범위 : </span>
                     <span style={{marginLeft: '5px'}}>
                         {editMode ?
-                            <RowFlexBox style={{marginLeft:'-5px'}}>
+                            <RowFlexBox style={{marginLeft: '-5px'}}>
                                 <Radio_Label>
                                     <InputType_Radio name='inputForm'
                                                      defaultChecked={!groupSchedule.privacy}
@@ -179,7 +197,7 @@ export const ScheduleDetail: React.FC<GroupScheduleProps> = ({scheduleDetail}) =
                                     <span style={{fontSize: '11px'}}>참가자</span>
                                 </Radio_Label>
                             </RowFlexBox>
-                            :groupSchedule.privacy ?"참여인원만":"전체"}
+                            : groupSchedule.privacy ? "참여인원만" : "전체"}
                     </span>
                 </SchedulePrivacy_Container>
                 <ScheduleMember_Container>
@@ -217,7 +235,7 @@ export const ScheduleDetail: React.FC<GroupScheduleProps> = ({scheduleDetail}) =
 
                 </ScheduleButton_Container>
                 {mapModal &&
-                    <ScheduleMap_Container>
+                    <ScheduleMap_Container id="map" ref={mapElement}>
                         대충 지도
                     </ScheduleMap_Container>
                 }
@@ -226,27 +244,27 @@ export const ScheduleDetail: React.FC<GroupScheduleProps> = ({scheduleDetail}) =
                                     subScheduleEdit={subScheduleEdit}
                                     dispatchSubSchedule={dispatchSubSchedule}
                                     startDate={new Date(groupSchedule.startDate)}/>
-                </ScheduleDetailScroll_Wrapper>
+            </ScheduleDetailScroll_Wrapper>
 
             {editMode &&
-            <ScheduleButton_Container style={{paddingTop:'5px',borderTop:`1px solid ${TextColor}77`}}>
-                <div>
-                    <Schedule_Button onClick={useSubSchedule.addSubSchedule}>
-                        세부일정 추가
-                    </Schedule_Button>
-                    <Schedule_Button onClick={saveGroupSchedule}>
-                        저장하기
-                    </Schedule_Button>
-                </div>
-                <Trash_Container onMouseEnter={useSubSchedule.deleteSubSchedule}>
-                    <Trash_Top $isDrag={useSubSchedule.dragIndex.current === null}>
-                        <TrashIconTop className="bi bi-trash-fill"></TrashIconTop>
-                    </Trash_Top>
-                    <Trash_Body>
-                        <TrashIconBottom className="bi bi-trash-fill"></TrashIconBottom>
-                    </Trash_Body>
-                </Trash_Container>
-            </ScheduleButton_Container>
+                <ScheduleButton_Container style={{paddingTop: '5px', borderTop: `1px solid ${TextColor}77`}}>
+                    <div>
+                        <Schedule_Button onClick={useSubSchedule.addSubSchedule}>
+                            세부일정 추가
+                        </Schedule_Button>
+                        <Schedule_Button onClick={saveGroupSchedule}>
+                            저장하기
+                        </Schedule_Button>
+                    </div>
+                    <Trash_Container onMouseEnter={useSubSchedule.deleteSubSchedule}>
+                        <Trash_Top $isDrag={useSubSchedule.dragIndex.current === null}>
+                            <TrashIconTop className="bi bi-trash-fill"></TrashIconTop>
+                        </Trash_Top>
+                        <Trash_Body>
+                            <TrashIconBottom className="bi bi-trash-fill"></TrashIconBottom>
+                        </Trash_Body>
+                    </Trash_Container>
+                </ScheduleButton_Container>
             }
         </ScheduleDetail_Container>
     )
