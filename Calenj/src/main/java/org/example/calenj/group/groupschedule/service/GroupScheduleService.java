9f5a66814346b0e65e55f3f8cf780d9b9023e47c
kwontaein <zodls1128@gmail.com>
2024-08-08 20:02:22 +0900

    08_08 #2

--- Calenj/src/main/java/org/example/calenj/group/groupschedule/service/GroupScheduleService.java
+++ Calenj/src/main/java/org/example/calenj/group/groupschedule/service/GroupScheduleService.java
@@ -234,7 +234,7 @@ public class GroupScheduleService {
      */
     private List<String> extractTagKey() {
         return calendarService.getTagEntityList().stream()
-                .filter(tag -> "group".equals(tag.getName()))
+                .filter(tag -> "그룹 일정".equals(tag.getName()))
                 .map(tag -> tag.getId().toString())
                 .findFirst()
                 .map(Collections::singletonList)
@@ -254,7 +254,7 @@ public class GroupScheduleService {
                 response.getSubScheduleId(), null, null, false, 1, null, null, null, null, 1, null, null);
 
         ExtendedPropsRequest extendedPropsRequest = new ExtendedPropsRequest(
-                tagKey, "schedule", response.getSubScheduleContent(), null, null, repeatStateRequest);
+                tagKey, "promise", response.getSubScheduleContent(), null, null, repeatStateRequest);
 
         Timestamp newEnd = new Timestamp(newStart.getTime() + (response.getSubScheduleDuration() * 3600000L));
 
@@ -273,16 +273,17 @@ public class GroupScheduleService {
 
         if (newList.contains(myUserName)) {
             newList.remove(myUserName);
-            response = "해당 일정에 참여하였습니다.";
+            response = "해당 일정에 참여를 취소했습니다.";
+            calendarService.deleteSchedule(subScheduleId);
         } else {
             newList.add(myUserName);
-            response = "해당 일정에 참여를 취소했습니다.";
+            response = "해당 일정에 참여하였습니다.";
+            addCalendar(groupScheduleRepository.findById(groupSubScheduleEntity.getScheduleId().getScheduleId()).orElse(null), subScheduleId);
         }
 
         //서브 일정에 유저 추가
         groupSubScheduleRepository.updateJoinUser(subScheduleId, newList.toString());
 
-        addCalendar(groupScheduleRepository.findById(groupSubScheduleEntity.getScheduleId().getScheduleId()).orElse(null), subScheduleId);
         return response;
     }
 }
