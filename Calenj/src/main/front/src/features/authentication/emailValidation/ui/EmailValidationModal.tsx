05a1018b0d2d25485063eaefda74fcb0682ef5ae
kwontaein <zodls1128@gmail.com>
2024-08-11 08:48:49 +0900

    08_09 로그인 ui 대충 제작

--- Calenj/src/main/front/src/features/authentication/emailValidation/ui/EmailValidationModal.tsx
+++ Calenj/src/main/front/src/features/authentication/emailValidation/ui/EmailValidationModal.tsx
@@ -1,4 +1,4 @@
-import {Input, FormLable} from '../../../../shared/ui/SharedStyled';
+import {Input, FormLable, ThemeColor2} from '../../../../shared/ui/SharedStyled';
 import {ChangeEvent, useEffect, useState} from 'react';
 import './Sign.scss'
 import {ComponentProps} from "../model/types";
@@ -8,25 +8,25 @@ import {CheckEmailAuthButton} from './CheckEmailAuthButton'
 import {useSelector} from "react-redux";
 import {RootState} from "../../../../entities/redux";
 
-export const EmailValidationModal: React.FC<ComponentProps> = ({email,onClose}) => {
+export const EmailValidationModal: React.FC<ComponentProps> = ({email, onClose}) => {
     const [code, setCode] = useState<string>('');
     const closeModal = closeEmailValidModal(onClose);
     const {codeValid} = useSelector((state: RootState) => state.emailValidation); // emailToken 상태 선택
 
     useEffect(() => {
-        if(codeValid){
+        if (codeValid) {
             onClose()
         }
     }, [codeValid]);
 
     return (
-        <div id='emailValidation_Box'>
+        <div id='emailValidation_Box' style={{backgroundColor: `${ThemeColor2}`}}>
             <div>
-                <FormLable style={{marginLeft:'8px'}}>이메일로 전송된 인증코드를 입력해주세요.</FormLable>
+                <FormLable style={{marginLeft: '8px'}}>이메일로 전송된 인증코드를 입력해주세요.</FormLable>
                 <button id="btn_cancleValid" onClick={closeModal}>x</button>
             </div>
             <br></br>
-            <div style={{fontSize:"15px", marginTop:"3px", marginLeft:"10px"}}>{email}로 인증요청함</div>
+            <div style={{fontSize: "15px", marginTop: "3px", marginLeft: "10px"}}>{email}로 인증요청함</div>
 
             <CodeValidTime onClose={onClose}/>
             <Input maxLength={10}
